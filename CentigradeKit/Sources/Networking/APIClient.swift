68fd5f209d7d05faae7c1d07fd4486ebba19686d
Juri Noga <juri.noga@gmail.com>
2017-04-01 14:17:37 +0300

    Implement basic current weather request and parsing.

--- CentigradeKit/Sources/Networking/APIClient.swift
+++ CentigradeKit/Sources/Networking/APIClient.swift
@@ -10,18 +10,59 @@ import Foundation
 import Alamofire
 import CoreLocation
 
+public enum ErrorType{
+  case noNetwork,wrongResponse,other
+}
+
 public final class APIClient{
   
   private static let baseURL = URL(string: "https://api.darksky.net/forecast")!
   private static let apiKey = "fe5ad50a5265afdb30d230872c6e2577"
   private static let apiURL = baseURL.appendingPathComponent(apiKey)
   
-  
-  
-  public static func requestForecast(for location : CLLocationCoordinate2D){
+  public static func requestForecast(for location : CLLocationCoordinate2D, language : String = "en", completion : @escaping (_ dataPoint : WeatherDataPoint?,_ error : ErrorType?)->()){
+    let url = requestURL(from: location)
+    let parameters : Parameters = ["lang" : "[\(language)]", "exclude" : "[alerts]"]
+    
+    Alamofire.request(url, method: .get, parameters: parameters).responseJSON { response in
+      
+      guard response.result.error == nil else {
+        completion(nil, response.result.error?.type)
+        return
+      }
+      guard let responseDict = response.result.value as? [String : AnyObject] else{
+        completion(nil, .wrongResponse)
+        return
+      }
+      guard let currentDict = responseDict["currently"] as? [String : AnyObject] else{
+        completion(nil, .wrongResponse)
+        return
+      }
+
+      let currentDataPoint = WeatherDataPoint(from: currentDict, location: location)
+      completion(currentDataPoint, nil)
+    }
     
     
   }
   
+  private static func requestURL(from location : CLLocationCoordinate2D)->URL{
+    let locationString = [String(location.latitude),String(location.longitude)].joined(separator: ",")
+    return apiURL.appendingPathComponent(locationString)
+  }
+  
+}
+
+extension Error{
+  
+  var type : ErrorType{
+    let code = (self as NSError).code
+    print("ERROR WITH CODE \(code)")
+    if code == -1009{
+      return .noNetwork
+    }else{
+      return .other
+    }
+  }
   
 }
