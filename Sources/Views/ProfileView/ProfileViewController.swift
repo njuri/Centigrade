4c588eacaf6ce370571a53420b3364c09dfa5af1
Juri Noga <juri.noga@gmail.com>
2017-04-06 18:20:36 +0300

    Implement editing profile info (not saved yet). Add base localization files.

--- Sources/Views/ProfileView/ProfileViewController.swift
+++ Sources/Views/ProfileView/ProfileViewController.swift
@@ -15,8 +15,9 @@ protocol ProfileCardControllerDelegate: class{
 
 final class ProfileViewController : UIViewController {
   
+  @IBOutlet weak var nameField: UITextField!
+  @IBOutlet weak var profilePlaceholderImageView: UIImageView!
   @IBOutlet weak var profileImageView: UIImageView!
-  @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var profileTableView: UITableView!
   
   @IBOutlet var automaticCell: UITableViewCell!
@@ -29,14 +30,21 @@ final class ProfileViewController : UIViewController {
   @IBOutlet weak var chevronIcon: UIImageView!
   @IBOutlet weak var chevronButton: UIButton!
   @IBOutlet weak var editProfileButton: UIButton!
+  @IBOutlet weak var addButton: UIButton!
   
   weak var delegate : ProfileCardControllerDelegate?
+  
+  var isEditingProfile = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     setupSegmentedControl()
     setupAutomaticCell()
+    editProfileButton.setTitle(NSLocalizedString("EDIT_PROFILE_BUTTON", comment: "Edit Profile"), for: .normal)
+    profilePlaceholderImageView.image = profilePlaceholderImageView.image?.withRenderingMode(.alwaysTemplate)
+    profilePlaceholderImageView.tintColor = .gray
+    nameField.placeholder = NSLocalizedString("YOUR_NAME_LABEL", comment: "Your Name")
   }
   
   override func viewDidLayoutSubviews() {
@@ -45,6 +53,63 @@ final class ProfileViewController : UIViewController {
   }
   
   @IBAction func editProfilePressed(_ sender: Any) {
+    if isEditingProfile {
+      toggleProfileEdit(isOn: false)
+    }else{
+      toggleProfileEdit(isOn: true)
+    }
+  }
+  
+  func toggleProfileEdit(isOn : Bool){
+    isEditingProfile = isOn
+    nameField.isEnabled = isOn
+    
+    let imageAlpha : CGFloat
+    let addButtonAlpha : CGFloat
+    if isOn{
+      addButton.isHidden = false
+      addButtonAlpha = 1
+      imageAlpha = 0.17
+      editProfileButton.setTitle(NSLocalizedString("DONE_BUTTON", comment: "Done"), for: .normal)
+      nameField.becomeFirstResponder()
+    }else{
+      addButtonAlpha = 0
+      imageAlpha = 1
+      editProfileButton.setTitle(NSLocalizedString("EDIT_PROFILE_BUTTON", comment: "Edit Profile"), for: .normal)
+      nameField.resignFirstResponder()
+    }
+    
+    UIView.animate(withDuration: 0.3, animations: {
+      self.profileImageView.alpha = imageAlpha
+      self.addButton.alpha = addButtonAlpha
+      self.profilePlaceholderImageView.alpha = imageAlpha
+    }) { _ in
+      self.addButton.isHidden = !isOn
+    }
+    
+    
+  }
+  
+  @IBAction func addImagePressed(_ sender: Any) {
+    let ac = UIAlertController(title: NSLocalizedString("PROFILE_IMAGE_LABEL", comment: "Profile image"), message: NSLocalizedString("PICTURE_QUESTION_LABEL", comment: "How do you want to add your profile pic?"), preferredStyle: .actionSheet)
+    
+    if UIImagePickerController.isSourceTypeAvailable(.camera){
+      let cameraAction = UIAlertAction(title: NSLocalizedString("TAKE_CAMERA_LABEL", comment: "Take using camera"), style: .default, handler: { (action) in
+        self.createImagePicker(with: .camera)
+      })
+      ac.addAction(cameraAction)
+    }
+    
+    if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+      let libraryAction = UIAlertAction(title: NSLocalizedString("FROM_LIBRARY_LABEL", comment: "Choose from library"), style: .default, handler: { (action) in
+        self.createImagePicker(with: .photoLibrary)
+      })
+      ac.addAction(libraryAction)
+    }
+    ac.addAction(UIAlertAction(title: NSLocalizedString("CANCEL_LABEL", comment: "Cancel"), style: .cancel, handler: nil))
+    
+    present(ac, animated: true, completion: nil)
+    
   }
   
   func setupAutomaticCell(){
@@ -92,9 +157,25 @@ final class ProfileViewController : UIViewController {
     }
     setupAutomaticCell()
   }
-
+  
   @IBAction func chevronPressed(_ sender: Any) {
     delegate?.dismissPressed()
+  }
+  
+  
+  
+  func createImagePicker(with type : UIImagePickerControllerSourceType){
+    guard UIImagePickerController.isSourceTypeAvailable(type) else { return }
+    let ivc = UIImagePickerController()
+    ivc.delegate = self
+    ivc.sourceType = type
+    ivc.allowsEditing = true
+    present(ivc, animated: true, completion: nil)
+  }
+  
+  func processSelectedImage(image : UIImage){
+    profilePlaceholderImageView.isHidden = true
+    profileImageView.image = image
   }
   
 }
@@ -130,5 +211,22 @@ extension ProfileViewController : UITableViewDelegate{
   
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     return "Temperature units"
+  }
+}
+
+extension ProfileViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage{
+      processSelectedImage(image: editedImage)
+    }else
+      if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+        processSelectedImage(image: originalImage)
+    }
+    
+    dismiss(animated: true, completion: nil)
   }
 }
